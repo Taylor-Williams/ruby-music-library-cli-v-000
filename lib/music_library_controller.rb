95bf131cbdf4ec6383c442ee398d46e13eae983d
Taylor Williams <taylor@thewilliams.net>
2018-06-11 22:07:00 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -52,7 +52,6 @@ class MusicLibraryController
   end
 
   def play_song
-    list_songs
     puts "Which song number would you like to play?"
     song = Song.all[gets.strip.to_i - 1]
     puts "Playing #{song.name} by #{song.artist.name}"
