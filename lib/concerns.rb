d5d0bb4c5b3381dff14720a087a9730e57ac7327
Taylor Williams <taylor@thewilliams.net>
2018-06-11 20:38:18 +0000

    automatically backed up by learn

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -5,7 +5,7 @@ module Concerns
     end
 
     def find_or_create_by_name(name)
-      self.create(name) if !self.find_by_name(name)
+      song = self.find_by_name(name) ? song : self.create(name)
     end
   end
 end
