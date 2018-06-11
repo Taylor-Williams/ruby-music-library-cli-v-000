8cd8453a5d2d9d4b9aea85c00a06edf9a9c41ec8
Taylor Williams <taylor@thewilliams.net>
2018-06-11 21:06:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,5 +17,9 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    command = gets.strip.downcase
+    while (command != "exit")
+
+    end
   end
 end
