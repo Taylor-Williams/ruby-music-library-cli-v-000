d529fec08dd570d7c0d49aab69e1801af6266ccf
Taylor Williams <taylor@thewilliams.net>
2018-06-11 21:07:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     command = gets.strip.downcase
-    while (command != "exit")
+    while command != "exit"
 
     end
   end
